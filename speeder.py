e80847d6605ef10ddb40d0332220762cf3fe7a12
playernam <truongxuannam@aptech.vn>
2025-01-09 22:53:37 +0700

    second update

--- speeder.py
+++ speeder.py
@@ -5,7 +5,7 @@ def log(s):
     L.write(s)
     L.write('\n')
 
-CNT = 1000
+CNT = 1000      # number of runs
 
 L = open('speeder.log', 'a')
 
@@ -16,7 +16,7 @@ log(f'{"min":^19} | {"max":^19} | {"avg":^19} | step')
 for r in range(CNT):
     t = time.time()
     a, b, c, d = 1, 0, 1.1, 1.01
-    for i in range(5_000_000):
+    for i in range(50000):
         a = (a * 123_456_789) % 987_654_321
         if a % 2: b = b + 1
         c = c / d
@@ -27,6 +27,6 @@ for r in range(CNT):
     if t < m0: m0 = t
     if t > m1: m1 = t
 
-    if r % 10 == 9:
-        log(f'{m0:19.15f} | {m1:19.15f} | {(s-m0-m1)/(r-1):19.15f} | {r+1}')
-open('finetest.py', 'a').write(f'SCALE = {(s-m0-m1)/(CNT-2)}\n')
+    log(f'{m0:19.15f} | {m1:19.15f} | {s/(r+1):19.15f} | {r+1}')
+
+open('finetest.py', 'a').write(f'SCALE = {(s-m0-m1) / (CNT-2) * 100}\n')
